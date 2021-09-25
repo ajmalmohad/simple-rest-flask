ec5b8ce119b23a629abfdc7b179566f80e308e32
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-09-25 14:38:42 +0530

    Stack Implemented

--- stack.py
+++ stack.py
@@ -2,3 +2,21 @@ class Node:
     def __init__(self,data,next_node):
         self.data = data
         self.next_node = next_node
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.next = None
+
+    def peek(self):
+        return self.top
+
+    def push(self,data):
+        self.top = Node(data,self.top)
+
+    def pop(self):
+        if self.top is None:
+            return None
+        removed = self.top
+        self.top = self.top.next_node
+        return removed
